343a757bc89530dd041008975a6210f782362e63
Ryanpluz <ryanfalcao007t@gmail.com>
2024-11-07 15:05:54 -0300

    Atividade_Individual

---
+++ Atividade individual/src/js/scripts.js
@@ -0,0 +1,5 @@
+// Highlights current date on contact page
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
