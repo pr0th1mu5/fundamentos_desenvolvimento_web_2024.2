a70c5b3fbbf27c7f71f7c8e17c105708d9525b4b
Ryanpluz <ryanfalcao007t@gmail.com>
2024-11-07 20:02:45 -0300

    Delete Atividade individual directory

--- Atividade individual/src/js/scripts.js
+++
@@ -1,5 +0,0 @@
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
